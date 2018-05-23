91fee7f4a3e43f648f699485bd32b98e22f68b99
jmenken <julia.menken@gmail.com>
2018-05-23 18:31:52 +0200

    small changes

--- Aufg5B_Ring/ring.f95
+++ Aufg5B_Ring/ring.f95
@@ -4,8 +4,8 @@ program ring
 	
 	implicit none
 	
-	integer :: ierr, rank, size	! error, processor id and num of processors
-	integer , dimension(MPI_STATUS_SIZE) :: status	! for MPI receiving
+	integer :: ierr, rank, size	! error, process id and num of process
+	integer :: status(MPI_STATUS_SIZE)	! for MPI receiving
 	integer, parameter :: msg = 12	! for MPI receiving and sending
 	integer :: sum_id	! updated sum of process ids
 	integer :: recp	! recipient of MPI sending
@@ -14,32 +14,34 @@ program ring
 	call MPI_INIT(ierr)
 	call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierr)
 	call MPI_COMM_SIZE (MPI_COMM_WORLD, size,ierr)
-	
-	! first processor starts the ring communication
+	print*, 'I am', rank, 'of', size
+
+	! first process starts the ring communication
 	if (rank==0) then
 		sum_id = rank
 		call MPI_Send(sum_id, 1, MPI_INTEGER, rank+1, msg, MPI_COMM_WORLD, ierr)
 		
-		! write the added up process ids after going through all processors and ending up
-		! again at the first processor
+		! write the added up process ids after going through all process and ending up
+		! again at the first process
 		call MPI_Recv(sum_id, 1, MPI_INTEGER, size-1, msg, MPI_COMM_WORLD, status, ierr)
-		write(*,*) "added up process ids: ", sum_id
+		write(*,*) "Added up process ids: ", sum_id
 	
 	else
-		! receive the current value of sum_id from the processor before
+		! receive the current value of sum_id from the process before
 		call MPI_Recv(sum_id, 1, MPI_INTEGER, rank-1, msg, MPI_COMM_WORLD, status, ierr)
+		print*, rank, "received"
 		
 		if (rank < size-1) then
-			! if it's not the last processor set the recipient to the next processor
+			! if it's not the last process set the recipient to the next process
 			recp = rank+1
 		else
-			! if it's the last processor set the recipient to the first processor
+			! if it's the last process set the recipient to the first process
 			recp = 0
-		
-		! send the current sum of ids plus the processor id
-		call MPI_Send(sum_id+rank, 1, MPI_INTEGER, recp, msg, MPI_COMM_WORLD, ierr)
-		
 		endif
+		
+		! send the current sum of ids plus the process id
+		call MPI_Send(sum_id+rank, 1, MPI_INTEGER, recp, msg, MPI_COMM_WORLD, ierr)
+
 	endif
 	
 	call MPI_FINALIZE(ierr)
