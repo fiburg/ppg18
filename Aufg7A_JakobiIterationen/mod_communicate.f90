248ea366bf097b9b2094aea8210463b7cc65297a
jmenken <julia.menken@gmail.com>
2018-06-18 16:14:57 +0200

    Added module for communication

---
+++ Aufg7A_JakobiIterationen/mod_communicate.f90
@@ -0,0 +1,76 @@
+! Paralleles Programmieren für Geowissenschaftler im SS 2018
+! Uebungsblatt 7
+! Abgabe 19.06.2018
+! Menken und Burgemeister
+
+!
+module communicate
+	use mpi
+	
+	implicit none
+	contains
+
+	! Kommunikation zwischen den Prozessen
+	subroutine sendrecvHalo(master, NDIM, iter, chunck, cdim, mpi_err, mpi_rank, mpi_size, mpi_req, status)
+		implicit none
+		real(kind=8), dimension(:,:), pointer, intent(inout) :: chunck
+		real(kind=8), dimension(:), pointer :: frow, lrow, efrow, elrow
+		integer, intent(in) :: master, iter, cdim, NDIM
+		integer, intent(in) :: mpi_err, mpi_rank, mpi_size, mpi_req, status(MPI_STATUS_SIZE)
+		
+		! first row, last row, e
+		allocate(frow(0:NDIM))
+		allocate(lrow(0:NDIM))
+		allocate(efrow(0:NDIM))
+		allocate(elrow(0:NDIM))
+	
+		frow(:) = chunck(:,1)
+		lrow(:) = chunck(:,cdim-1)
+
+		if (mpi_rank == master) then
+			call MPI_ISEND(lrow, size(lrow), MPI_REAL8, mpi_rank+1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			
+			call MPI_IRECV(elrow, size(elrow), MPI_REAL8, mpi_rank+1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			
+			chunck(:,cdim) = elrow(:) ! untere Randzeile
+		else if (mpi_rank == mpi_size-1) then
+			call MPI_ISEND(frow, size(frow), MPI_REAL8, mpi_rank-1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			
+			call MPI_IRECV(efrow, size(efrow), MPI_REAL8, mpi_rank-1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			
+			chunck(:,0) = efrow(:) ! obere Randzeile
+		else
+			call MPI_ISEND(frow, size(frow), MPI_REAL8, mpi_rank-1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			call MPI_ISEND(lrow, size(lrow), MPI_REAL8, mpi_rank+1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			
+			call MPI_IRECV(efrow, size(efrow), MPI_REAL8, mpi_rank-1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+			call MPI_IRECV(elrow, size(elrow), MPI_REAL8, mpi_rank+1, iter, &
+			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
+			call MPI_WAIT(mpi_req, status, mpi_err)
+	
+			chunck(:,cdim) = elrow(:) ! obere Randzeile
+			chunck(:,0) = efrow(:) ! untere Randzeile
+		end if
+		
+		deallocate(elrow)
+		deallocate(efrow)
+		deallocate(frow)
+		deallocate(lrow)
+	
+	end subroutine
+
+end module communicate
--- Aufg7A_JakobiIterationen/poisson.f90
+++ Aufg7A_JakobiIterationen/poisson.f90
@@ -2,138 +2,87 @@ program Poisson
 	use mpi
 	use initialize
 	use run
+	use communicate
 	use finalize
 
 	implicit none
 
-	real(kind=8), dimension(:,:), pointer :: matrix, chunck
-	real(kind=8), dimension(:), pointer :: frow, lrow, efrow, elrow
+	real(kind=8), dimension(:,:), pointer :: matrix, chunck	! Matrix, Teilmatrix
+	real(kind=8), dimension(:), pointer :: frow, lrow, efrow, elrow	! Halo lines
+	integer, parameter :: NDIM = 96	! Dimension der Matrix
+	integer, parameter :: interlines = 11	! Anzahl Interlines
+	integer, parameter :: NITER = 100000	! Anzahl Iterationen
 	integer, dimension(:), pointer :: sendcounts, displacement
-	integer, parameter :: NDIM = 184
-	integer, parameter :: interlines = 22
-	integer, parameter :: NITER = 400000	! Anzahl Iterationen
-	integer, parameter :: master = 0	
-	integer :: iter, cdim
+	integer, parameter :: master = 0	! Master Prozess
+	integer :: iter, cdim	! Anzahl der ausgeführten Iterationen, Dimension des Chuncks
 	integer :: mpi_err, mpi_rank, mpi_size
 	integer :: mpi_req, status(MPI_STATUS_SIZE)
-
+	
+	! MPI Initialisierung
 	call MPI_INIT(mpi_err)
 	call MPI_COMM_RANK(MPI_COMM_WORLD,mpi_rank,mpi_err)
 	call MPI_COMM_SIZE(MPI_COMM_WORLD,mpi_size,mpi_err)
-
+	
+	! Allokation, Initialisierung, erstes Ausgeben der Matrix
 	if (mpi_rank == master) then
 		call createMatrix(matrix, NDIM, NDIM)
 		call initializeMatrix(matrix)
 		call outputMatrix(matrix, interlines, 0)
 	end if
-
+	
+	! Festlegen der Aufteilung der Matrix in Teilmatrizen
 	call initSequence(sendcounts, displacement, NDIM+1, mpi_size)
 
 	! Erweitere chunck fuer Randzeilen, der erste chunck hat nur eine
 	! untere Randzeile, der letzte nur eine obere Randzeile
 	if (mpi_rank == master .or. mpi_rank == mpi_size-1) then
-		cdim = sendcounts(mpi_rank+1) / NDIM - 1 + 1
+		cdim = sendcounts(mpi_rank+1) / (NDIM+1) - 1 + 1	! eine Randzeile
 	else
-		cdim = sendcounts(mpi_rank+1) / NDIM - 1 + 2
+		cdim = sendcounts(mpi_rank+1) / (NDIM+1) - 1 + 2	! zwei Randzeilen
 	end if
 	
+	! Allokation der Teilmatrix
 	call createMatrix(chunck, NDIM, cdim)
 
-	! Verteilung der Matrix auf chuncks
+	! Verteilung der Matrix auf Teilmatrix
 	if (mpi_rank == master) then
-		call MPI_SCATTERV(matrix, sendcounts, displacement, MPI_INTEGER, &
-		&		  chunck(:,0:cdim-1), sendcounts(mpi_rank+1), MPI_INTEGER, &
+		call MPI_SCATTERV(matrix, sendcounts, displacement, MPI_REAL8, &
+		&		  chunck(:,0:cdim-1), sendcounts(mpi_rank+1), MPI_REAL8, &
 		&		  master, MPI_COMM_WORLD, mpi_err)
 	else if (mpi_rank == mpi_size-1) then
-		call MPI_SCATTERV(matrix, sendcounts, displacement, MPI_INTEGER, &
-		&		  chunck(:,1:), sendcounts(mpi_rank+1), MPI_INTEGER, &
+		call MPI_SCATTERV(matrix, sendcounts, displacement, MPI_REAL8, &
+		&		  chunck(:,1:), sendcounts(mpi_rank+1), MPI_REAL8, &
 		&		  master, MPI_COMM_WORLD, mpi_err)
 	else
-		call MPI_SCATTERV(matrix, sendcounts, displacement, MPI_INTEGER, &
-		&		  chunck(:,1:cdim-1), sendcounts(mpi_rank+1), MPI_INTEGER, &
+		call MPI_SCATTERV(matrix, sendcounts, displacement, MPI_REAL8, &
+		&		  chunck(:,1:cdim-1), sendcounts(mpi_rank+1), MPI_REAL8, &
 		&		  master, MPI_COMM_WORLD, mpi_err)
 	end if
-
-	allocate(frow(0:NDIM))
-	allocate(lrow(0:NDIM))
-	allocate(efrow(0:NDIM))
-	allocate(elrow(0:NDIM))
-
-
-	do iter=1,NITER
-
-		frow(:) = chunck(:,1)
-		lrow(:) = chunck(:,cdim-1)
-
-		print*,shape(chunck)
-
-		if (mpi_rank == master) then
-			call MPI_ISEND(lrow, size(lrow), MPI_REAL, mpi_rank+1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			
-			call MPI_IRECV(elrow, size(elrow), MPI_REAL, mpi_rank+1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			
-			chunck(:,cdim) = elrow(:)
-		else if (mpi_rank == mpi_size-1) then
-			call MPI_ISEND(frow, size(frow), MPI_REAL, mpi_rank-1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			
-			call MPI_IRECV(efrow, size(efrow), MPI_REAL, mpi_rank-1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			
-			chunck(:,0) = efrow(:)
-		else
-			call MPI_ISEND(frow, size(frow), MPI_REAL, mpi_rank-1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			call MPI_ISEND(lrow, size(lrow), MPI_REAL, mpi_rank+1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			
-			call MPI_IRECV(efrow, size(efrow), MPI_REAL, mpi_rank-1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
-			call MPI_IRECV(elrow, size(elrow), MPI_REAL, mpi_rank+1, iter, &
-			&	       MPI_COMM_WORLD, mpi_req, mpi_err)
-			call MPI_WAIT(mpi_req, status, mpi_err)
 	
-			chunck(:,cdim) = elrow(:)
-			chunck(:,0) = efrow(:)
-		end if
-
+	! Iteration Jacobi Berechnung
+	do iter=1,NITER
+		call sendrecvHalo(master, NDIM, iter, chunck, cdim, mpi_err, mpi_rank, mpi_size, mpi_req, status)
 		call calculate(chunck)
 
 	end do
 
-	deallocate(elrow)
-	deallocate(efrow)
-	deallocate(frow)
-	deallocate(lrow)
-
 	if (mpi_rank == master) then
-		call MPI_GATHERV(chunck(:,0:cdim-1), sendcounts(mpi_rank+1), MPI_INTEGER, &
-		&		 matrix, sendcounts, displacement, MPI_INTEGER, &
+		call MPI_GATHERV(chunck(:,0:cdim-1), sendcounts(mpi_rank+1), MPI_REAL8, &
+		&		 matrix, sendcounts, displacement, MPI_REAL8, &
 		&		 master, MPI_COMM_WORLD, mpi_err)
 	else if (mpi_rank == mpi_size-1) then
-		call MPI_GATHERV(chunck(:,1:), sendcounts(mpi_rank+1), MPI_INTEGER, &
-		&		 matrix, sendcounts, displacement, MPI_INTEGER, &
+		call MPI_GATHERV(chunck(:,1:), sendcounts(mpi_rank+1), MPI_REAL8, &
+		&		 matrix, sendcounts, displacement, MPI_REAL8, &
 		&		 master, MPI_COMM_WORLD, mpi_err)
 	else
-		call MPI_GATHERV(chunck(:,1:cdim-1), sendcounts(mpi_rank+1), MPI_INTEGER, &
-		&		 matrix, sendcounts, displacement, MPI_INTEGER, &
+		call MPI_GATHERV(chunck(:,1:cdim-1), sendcounts(mpi_rank+1), MPI_REAL8, &
+		&		 matrix, sendcounts, displacement, MPI_REAL8, &
 		&		 master, MPI_COMM_WORLD, mpi_err)
 	end if
 	
 	call freeMatrix(chunck)
 	
 	if (mpi_rank == master) then
-
-		print*, matrix(:,0)
 		call outputMatrix(matrix, interlines, iter-1)
 		call freeMatrix(matrix)
 	end if
